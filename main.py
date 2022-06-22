8f3604e3abd2c88f098e664a39031c688ecc7d39
sushilc147 <sushilc147@gmail.com>
2022-06-22 21:46:21 +0545

    add pa_artifacts and va_artifacts

--- main.py
+++ main.py
@@ -1,17 +1,75 @@
 from artifactory import ArtifactoryPath
 import settings
+import csv
+import json
 
 # API Key
-path = ArtifactoryPath(f"{settings.URL}/", auth=(settings.USERNAME, settings.PASSWORD))
-print(path)
+pa_path = ArtifactoryPath(settings.PA_URL, auth=(settings.USERNAME, settings.PASSWORD))
+va_path = ArtifactoryPath(settings.VA_URL, auth=(settings.USERNAME, settings.PASSWORD))
+print(pa_path)
+print(va_path)
 
 # ''items.find({"type":"file"}).sort({"$desc": ["size"]}).limit(100) '
-artifacts = path.aql('items.find', {"type": "file"}, '.sort', {'$desc': ['size']},  '.limit', '100')
+aql_args = [
+    'items.find',
+    {"type": "file"},
+    '.sort',
+    {'$desc': ['size']},
+    '.limit',
+    '100'
+]
 
-# aql_args = [
-#     'items.find',{
-#
+pa_artifacts = pa_path.aql(*aql_args)
+print(pa_artifacts)
+
+va_artifacts = va_path.aql(*aql_args)
+print(va_artifacts)
+
+"""
+The output is assumed to be in the format below
+To test on actual device uncomment codes above
+"""
+# pa_artifacts = """
+# {
+#     "storageSummary": {
+#         "repositoriesSummaryList": [
+#             {
+#                 "repoKey": "DATA HERE",
+#                 "repoType": "DATA HERE",
+#                 "foldersCount": 10,
+#                 "filesCount": 2,
+#                 "usedSpace": "10 GB",
+#                 "itemsCount": 2,
+#                 "packageType": "Npm",
+#                 "percentage": "0%"
+#             },
+#             {
+#                 "repoKey": "DATA 2 HERE",
+#                 "repoType": "DATA 2 HERE",
+#                 "foldersCount": 20,
+#                 "filesCount": 200,
+#                 "usedSpace": "10 GB",
+#                 "itemsCount": 220,
+#                 "packageType": "Npm",
+#                 "percentage": "10%"
+#             }
+#         ]
 #     }
-# ]
+# }
+# """
+pa_artifacts = json.loads(pa_artifacts)
+va_artifacts = json.loads(va_artifacts)
+field_names = (
+    'repoKey', 'repoType', 'foldersCount', 'filesCount', 'usedSpace', 'itemsCount', 'packageType', 'percentage'
+)
 
-print(artifacts)
+with open('pa_output.csv', 'w') as file:
+    writer = csv.DictWriter(file, field_names)
+    writer.writeheader()
+    writer.writerows(pa_artifacts["storageSummary"]["repositoriesSummaryList"])
+
+
+with open('va_output.csv', 'w') as file:
+    writer = csv.DictWriter(file, field_names)
+    writer.writeheader()
+    writer.writerows(va_artifacts["storageSummary"]["repositoriesSummaryList"])
--- settings.py
+++ settings.py
@@ -4,4 +4,5 @@ from decouple import config
 USERNAME = config("ARTIFACTORY_USERNAME", cast=str)
 PASSWORD = config("ARTIFACTORY_PASSWORD", cast=str)
 
-URL = config("ARTIFACTORY_URL", default="https://artifactory.hrzon.as.bankof.com/artifactory/apisearch/aql", cast=str)
+PA_URL = config("ARTIFACTORY_URL_PA", default="https://artifactory.hrzon.as.bankof.com/artifactory", cast=str)
+VA_URL = config("ARTIFACTORY_URL_VA", default="https://artifactory.hrzon.as.bankof.com/artifactory", cast=str)
